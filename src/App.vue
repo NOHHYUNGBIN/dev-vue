2181099f43ae524afceea1b3f95d010cec371008
nohhyungbin <shgudqlsz@naver.com>
2025-03-03 18:23:47 +0900

    onClick

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,29 @@
 import { ref } from "vue";
 
 const menus = ref(["역삼동원룸", "천호동원룸", "봉천동원룸"]);
+const count = ref(0)
+const info = ref([{
+  id:'shgudqlsz',
+  name: '노형빈'
+}])
+const test = (props) => {
+  console.debug(props)
+  info.value.push({id:'ㅋㅋ', name:'이상경'})
+}
 </script>
 
 <template>
   <div v-for="menu in menus" :key="menu">
     <a>{{ menu }}</a>
+    <button @click="test('안녕녕')">허위매물신고</button><span>신고수 : {{count}}</span>
+  </div>
+  <div v-for="data in info" :key="data.id">
+    <p>
+      {{ data.id }}
+    </p>
+    <p>
+      {{ data.name }}
+    </p>
   </div>
 </template>
 
