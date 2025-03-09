ec442c80a69423f51208574b24b60139011cc8f3
NOHHYUNGBIN <shgudqlsz@naver.com>
2025-03-09 18:16:55 +0900

    react의 useRef처럼..ref사용하기

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -4,8 +4,16 @@
       <img :src="room.image" alt="Room Image" style="width: 100%" />
       <h4>{{ room.title }}</h4>
       <p>{{ room.content }}</p>
-      <input v-model="month" />
-      <p>{{ month }}개월 {{ room.price * month }}</p>
+      <input
+        ref="inputRef"
+        v-input="month"
+        placeholder="개월 수 입력(최대24개월)"
+        @input="debouncedValidateMonth"
+      />
+      <p>
+        {{ month ? month : 1 }}개월
+        {{ month ? room.price * month : room.price }}
+      </p>
       <button @click="emit('update:modal', { room: {}, isOpen: false })">
         닫기
       </button>
@@ -15,30 +23,55 @@
 
 <script setup>
 import { ref, watch } from "vue";
-const month = ref(1);
+const month = ref(null);
+const inputRef = ref(null);
 const props = defineProps({
   room: Object,
   modal: Boolean,
 });
 const emit = defineEmits(["update:modal"]);
 
-let debounce = null;
+let debounceTimer = null;
 
-watch(month, (newVal, oldVal) => {
-  clearTimeout(debounce);
-  debounce = setTimeout(() => {
-    if (isNaN(newVal)) {
-      alert("숫자만 입력 가능합니다.");
-      month.value = 1;
-      return;
-    }
-    if (newVal > 24 || newVal < 1) {
-      alert("1개월이상 24개월 이하만 입력 가능합니다.");
-      month.value = 1;
-      return;
-    }
+const validateMonth = (e) => {
+  let value = e.target.value;
+
+  if (isNaN(value)) {
+    inputRef.value.value = null;
+    alert("숫자만 입력 가능합니다.");
+    return;
+  }
+  if (value > 24 || value < 1) {
+    inputRef.value.value = null;
+    alert("1개월이상 24개월 이하만 입력 가능합니다.");
+    return;
+  }
+  month.value = value;
+};
+const debouncedValidateMonth = (e) => {
+  clearTimeout(debounceTimer); // 기존 타이머 삭제
+  debounceTimer = setTimeout(() => {
+    validateMonth(e); // 500ms 후 실행
   }, 500);
+};
+watch(month, (newVal) => {
+  console.debug("newValnewVal", newVal);
 });
+// watch(month, (newVal, oldVal) => {
+//   clearTimeout(debounce);
+//   debounce = setTimeout(() => {
+//     if (isNaN(newVal)) {
+//       alert("숫자만 입력 가능합니다.");
+//       month.value = 1;
+//       return;
+//     }
+//     if (newVal > 24 || newVal < 1) {
+//       alert("1개월이상 24개월 이하만 입력 가능합니다.");
+//       month.value = 1;
+//       return;
+//     }
+//   }, 500);
+// });
 </script>
 
 <style>
