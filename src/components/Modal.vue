8ca51eff2ad843e188f9ace0afa8c9afbb9f6932
NOHHYUNGBIN <shgudqlsz@naver.com>
2025-03-09 17:49:50 +0900

    watch + debounce

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -4,7 +4,8 @@
       <img :src="room.image" alt="Room Image" style="width: 100%" />
       <h4>{{ room.title }}</h4>
       <p>{{ room.content }}</p>
-      <p>{{ room.price }}</p>
+      <input v-model="month" />
+      <p>{{ month }}개월 {{ room.price * month }}</p>
       <button @click="emit('update:modal', { room: {}, isOpen: false })">
         닫기
       </button>
@@ -13,11 +14,31 @@
 </template>
 
 <script setup>
+import { ref, watch } from "vue";
+const month = ref(1);
 const props = defineProps({
   room: Object,
   modal: Boolean,
 });
 const emit = defineEmits(["update:modal"]);
+
+let debounce = null;
+
+watch(month, (newVal, oldVal) => {
+  clearTimeout(debounce);
+  debounce = setTimeout(() => {
+    if (isNaN(newVal)) {
+      alert("숫자만 입력 가능합니다.");
+      month.value = 1;
+      return;
+    }
+    if (newVal > 24 || newVal < 1) {
+      alert("1개월이상 24개월 이하만 입력 가능합니다.");
+      month.value = 1;
+      return;
+    }
+  }, 500);
+});
 </script>
 
 <style>
